cdabce30ea8f42041d9a157664f63becea69e0e0
Anastasia Osinska <60783750+AnastasiaOsinska@users.noreply.github.com>
2020-06-27 17:55:24 +0300

    Initial Commit

---
+++ FooodTr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FooodTr
+//
+//  Created by Anastasiya Osinskaya on 6/27/20.
+//  Copyright © 2020 Anastasiya Osinskaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
